1630b87a4ff0cb8a8795903d0639fb1c3f9d9f33
FredTheFork <Noelkuswik50@gmail.com>
2026-05-14 16:12:38 +0100

    Update AdditionalServicesPage.jsx

--- src/pages/AdditionalServicesPage.jsx
+++ src/pages/AdditionalServicesPage.jsx
@@ -31,6 +31,7 @@ function PageHeader() {
             fontSize: '0.75rem',
             letterSpacing: '0.15em',
             color: 'rgba(255,255,255,0.7)',
+            marginTop: '40px',
           }}
         >
           ADDITIONAL SERVICES
