2ab3a221154b6898a2ea537bd2e724e974e47334
FredTheFork <Noelkuswik50@gmail.com>
2026-05-21 22:27:11 +0100

    Update ContactPage.tsx

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -29,7 +29,7 @@ function PageHeader() {
             fontSize: '0.75rem',
             letterSpacing: '0.15em',
             color: 'rgba(255,255,255,0.7)',
-            marginTop: '0px',
+            marginTop: '72px',
           }}
         >
           GET IN TOUCH
